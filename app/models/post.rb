0718cb1b86faad1b7aa332c90e7312eb332bfdd6
Iana <midori.no.risu@gmail.com>
2015-11-10 23:07:35 +0200

    added welcome page and post entity with validation

---
+++ app/models/post.rb
@@ -0,0 +1,11 @@
+class Post < ActiveRecord::Base
+  validates :title,
+            presence: true,
+            length: {
+                in: 5..100,
+                too_short: "must be at least %{count} symbols long",
+                too_long: "must be at most %{count} symbols long"
+            },
+            uniqueness: { case_sensitive: false, message: 'should be unique' }
+  validates :body, presence: true
+end
